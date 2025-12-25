5932036e97bc9447f36b9d8ddb514fd2d49aeec5
Shekhhar <your-github-email@example.com>
2025-12-25 16:35:15 +0530

    Initial React project

---
+++ my-first-app/src/Content.jsx
@@ -0,0 +1,24 @@
+import "./content.css";
+
+const Content = () => {
+
+    return(
+        <>
+            <div className="container">
+                <div className="textual">
+                    <h2>esteem<br/>lab.</h2>
+                    <h6>PRESENTING</h6>
+                    <h1>UI & UX Dev<br/>Webinar</h1>
+                    <p>The earth for imperdiet laughter ultricies in the result of the diam of the orci potenti velit, said course put the throat to adorn.</p>
+                    <button id="btn1">Reserve My Seat</button>
+                    <button id="btn2">{">"} Learn More</button>
+                </div>
+                <div className="image">
+                    <img src="src/assets/download (5).jpg" />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Content;
